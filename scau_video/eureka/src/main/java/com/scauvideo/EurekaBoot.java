271ea4bbe4343552445b38215e770a0965332d83
zhanxiongjun <zhanj0123@gmail.com>
2020-11-07 08:55:52 +0800

    新建了一个新的SpringBoot工程，并将其改造成为一个新的父子工程，还在里面新生成了一个eureka微服务，注意里面的依赖问题，特别是dependencyManagement和dependency的问题。

---
+++ scau_video/eureka/src/main/java/com/scauvideo/EurekaBoot.java
@@ -0,0 +1,11 @@
+package com.scauvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaBoot.class);
+    }
+}
