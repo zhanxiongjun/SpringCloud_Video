350bd6ac8373e80b7c1cac433c87decba1384805
zhanxiongjun <zhanj0123@gmail.com>
2020-11-07 13:28:19 +0800

    给eureka添加了一个日志，以后应该就用这个模板啦

--- scau_video/eureka/src/main/java/com/scauvideo/EurekaBoot.java
+++ scau_video/eureka/src/main/java/com/scauvideo/EurekaBoot.java
@@ -1,11 +1,22 @@
 package com.scauvideo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaBoot {
+    private static final Logger LOG = LoggerFactory.getLogger(EurekaBoot.class);
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaBoot.class);
+        // ConfigurableEnvironment environment = app.run(args).getEnvironment();
+        LOG.info("注册中心项目启动成功！");
+        // LOG.info("注册中心地址为：127.0.0.1" + environment.getProperty("server.port"));
     }
 }
