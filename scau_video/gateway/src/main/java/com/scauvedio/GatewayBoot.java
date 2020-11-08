3669fd11731700b3586b6f657b404f2922b9e2ec
zhanxiongjun <zhanj0123@gmail.com>
2020-11-08 22:48:50 +0800

    这次真的太难了我，刚开始gateway依赖网络下载不好，所以得去本地仓库删除，然后再把starter——web依赖删掉才能正确运行。

--- scau_video/gateway/src/main/java/com/scauvedio/GatewayBoot.java
+++ scau_video/gateway/src/main/java/com/scauvedio/GatewayBoot.java
@@ -4,7 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -16,4 +19,12 @@ public class GatewayBoot {
         SpringApplication.run(GatewayBoot.class);
         log.info("GateWay网关启动成功啦");
     }
+    /*@Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/system/test")
+                        .uri("http://localhost:8881"))
+                .build();
+    }*/
+
 }
